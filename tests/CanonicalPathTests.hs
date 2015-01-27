527c961114c876d69dba9d8b3868e4cc85cde1bd
d12frosted <boris.buliga@icloud.com>
2015-01-27 23:17:21 +0200

    cleanup tests

--- tests/CanonicalPathTests.hs
+++ tests/CanonicalPathTests.hs
@@ -21,21 +21,23 @@ testCanonicalPath =
 testConstructor :: Test
 testConstructor =
   assertions "canonicalPathE tests" $
-  do $expect $ equal' (canonicalize "dir/") (good "dir")
-     $expect $ equal' (canonicalize "file") (good "file")
-     $expect $ equal' (canonicalize "file/") (good "file")
-     $expect $ equal' (canonicalize "dir/file1.txt") (good "dir/file1.txt")
-     $expect $ equal' (canonicalize "dir/ab/") (good "dir/ab")
-     $expect $ equal' (canonicalize "dir/file") (good "dir/file")
-     $expect $ equal' (canonicalize "dir/ab/../file") (good "dir/file")
-     $expect $ equal' (canonicalize "dir/ab/../ab/") (good "dir/ab")
-     $expect $ equal' (canonicalize "dir/ab/./file2.txt") (good "dir/ab/file2.txt")
-     $expect $ equal' (canonicalize "dir/ab/../../dir/ab/./../file1.txt") (good "dir/file1.txt")
-     $expect $ equal' (canonicalize deepFile1) (good deepFile1)
-     $expect $ equal' (canonicalize "diro/") (bad "diro" notExistError)
-     $expect $ equal' (canonicalize "dir.txt") (bad "dir.txt" notExistError)
-     $expect $ equal' (canonicalize "dir/ab/../../dir/ab/./../file1.tx") (bad "dir/file1.tx" notExistError)
-     $expect $ equal' (canonicalize deepFile2) (bad deepFile2 pathToLongError)
+  do let left' v = v >>= \v' -> return $ left v'
+         right' v = v >>= \v' -> return $ right v'
+
+     $expect $ right' (canonicalize "dir/")
+     $expect $ right' (canonicalize "file")
+     $expect $ right' (canonicalize "file/")
+     $expect $ right' (canonicalize "dir/file1.txt")
+     $expect $ right' (canonicalize "dir/ab/")
+     $expect $ right' (canonicalize "dir/file")
+     $expect $ right' (canonicalize "dir/ab/../file")
+     $expect $ right' (canonicalize "dir/ab/../ab/")
+     $expect $ right' (canonicalize "dir/ab/./file2.txt")
+     $expect $ right' (canonicalize "dir/ab/../../dir/ab/./../file1.txt")
+     $expect $ left' (canonicalize "diro/")
+     $expect $ left' (canonicalize "dir.txt")
+     $expect $ left' (canonicalize "dir/ab/../../dir/ab/./../file1.tx")
+     $expect $ left' (canonicalize deepFile)
 
 -- Helper functions
 
@@ -44,33 +46,8 @@ canonicalize p =
   do h <- currentDir
      liftM (Arrow.right unsafePath) . canonicalPathE $ (h </> p)
 
-good :: FilePath -> IO (Either Text FilePath)
-good p =
-  do h <- currentDir
-     return . Right $ (h </> p)
-
-bad :: FilePath -> Text -> IO (Either Text FilePath)
-bad p e =
-  do h <- currentDir
-     return . Left $ (either id id . toText $ h </> p) ++ e
-
-equal' :: (Show a, Eq a) => IO a -> IO a -> IO Assertion
-equal' a b =
-  do a' <- a
-     b' <- b
-     return $ equal a' b'
-
 currentDir :: IO FilePath
 currentDir = liftM ((</> "tests") . unsafePath) getCurrentDirectory
 
-deepFile1 :: FilePath
-deepFile1 = "dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-file.txt"
-
-deepFile2 :: FilePath
-deepFile2 = "dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-dir/deep-file.txt"
-
-notExistError :: Text
-notExistError = ": canonicalizePath: does not exist (No such file or directory)"
-
-pathToLongError :: Text
-pathToLongError = ": canonicalizePath: invalid argument (File name too long)"
+deepFile :: FilePath
+deepFile = concat $ replicate 500 "deep-hell"
