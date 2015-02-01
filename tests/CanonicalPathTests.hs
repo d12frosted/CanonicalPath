092b540bea672ec4af9e96a64c2e9b633fb6836f
d12frosted <boris.buliga@icloud.com>
2015-02-01 14:48:43 +0200

    update tests

--- tests/CanonicalPathTests.hs
+++ tests/CanonicalPathTests.hs
@@ -22,32 +22,45 @@ testConstructor :: Test
 testConstructor =
   assertions "canonicalPathE tests" $
   do let left' v = v >>= \v' -> return $ left v'
-         right' v = v >>= \v' -> return $ right v'
+         equal' v w = do
+           v' <- v
+           w' <- w
+           return $ equal v' w'
+         check p1 p2 = equal' (canonicalize' p1) (liftM Right $ inCurrentDir p2)
 
-     $expect $ right' (canonicalize "dir/")
-     $expect $ right' (canonicalize "file")
-     $expect $ right' (canonicalize "file/")
-     $expect $ right' (canonicalize "dir/file1.txt")
-     $expect $ right' (canonicalize "dir/ab/")
-     $expect $ right' (canonicalize "dir/file")
-     $expect $ right' (canonicalize "dir/ab/../file")
-     $expect $ right' (canonicalize "dir/ab/../ab/")
-     $expect $ right' (canonicalize "dir/ab/./file2.txt")
-     $expect $ right' (canonicalize "dir/ab/../../dir/ab/./../file1.txt")
-     $expect $ left' (canonicalize "diro/")
-     $expect $ left' (canonicalize "dir.txt")
-     $expect $ left' (canonicalize "dir/ab/../../dir/ab/./../file1.tx")
-     $expect $ left' (canonicalize deepFile)
+     $expect $ left' (canonicalize' "diro/")
+     $expect $ left' (canonicalize' "dir.txt")
+     $expect $ left' (canonicalize' "dir/ab/../../dir/ab/./../file1.tx")
+     $expect $ left' (canonicalize' deepFile)
+
+     $expect $ check "dir" "dir"
+     $expect $ check "file/" "file"
+     $expect $ check "dir/file1.txt" "dir/file1.txt"
+     $expect $ check "dir/ab/" "dir/ab"
+     $expect $ check "dir/file" "dir/file"
+     $expect $ check "dir/ab/../file" "dir/file"
+     $expect $ check "dir/ab/../ab/" "dir/ab"
+     $expect $ check "dir/ab/./file2.txt" "dir/ab/file2.txt"
+     $expect $ check "dir/ab/../../dir/ab/./../file1.txt" "dir/file1.txt"
+
+     $expect $ equal' (canonicalize "$HOME") (liftM Right getHomeDirectory)
+     $expect $ equal' (canonicalize "~/") (liftM Right getHomeDirectory)
 
 -- Helper functions
 
-canonicalize :: FilePath -> IO (Either Text FilePath)
-canonicalize p =
-  do h <- currentDir
-     liftM (Arrow.right unsafePath) . canonicalPathE $ (h </> p)
+canonicalize :: FilePath -> IO (Either Text CanonicalPath)
+canonicalize = canonicalPathE
+
+canonicalize' :: FilePath -> IO (Either Text FilePath)
+canonicalize' p = inCurrentDir p >>= liftM (Arrow.right unsafePath) . canonicalize
 
 currentDir :: IO FilePath
 currentDir = liftM ((</> "tests") . unsafePath) getCurrentDirectory
+
+inCurrentDir :: FilePath -> IO FilePath
+inCurrentDir p =
+  do h <- currentDir
+     return $ h </> p
 
 deepFile :: FilePath
 deepFile = concat $ replicate 500 "deep-hell"
